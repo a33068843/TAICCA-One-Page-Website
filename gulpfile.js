51fd96afb1d6815ef9351d40344f8f5fc4fa2336
Benny Jou <a33068843@gmail.com>
2021-11-18 23:13:48 +0800

    chore: 啟動專案

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const gulp        = require('gulp');
+const pug         = require('gulp-pug');
+const sass        = require('gulp-sass')(require('sass'));
+const watch       = require('gulp-watch');
+const plumber     = require('gulp-plumber');
+const browserSync = require('browser-sync').create();
+const rename      = require('gulp-rename');
+const changed     = require('gulp-changed');
+const imagemin    = require('gulp-imagemin');
+const uglify      = require('gulp-uglify');
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './www',
+      index: 'home.html'
+    },
+    port: 1234
+  })
+
+  gulp.watch('src/**/*.pug', gulp.series('pug'));
+  gulp.watch('src/**/*.scss', gulp.series('styles'));
+  gulp.watch('src/**/*.js', gulp.series('script'));
+  gulp.watch('src/images/*', gulp.series('images'));
+})
+
+gulp.task('pug', (done) => {
+  gulp
+    .src('src/**/[^_]*.pug')
+    .pipe(plumber())
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('./www'))
+    .pipe(browserSync.reload( {stream: true} ))
+  done();
+});
+
+gulp.task('styles', (done) => {
+  gulp
+    .src('src/**/[^_]*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('./www/css'))
+    .pipe(browserSync.reload( {stream: true} ))
+  done();
+});
+
+gulp.task('script', (done) => {
+  gulp
+    .src('src/**/[^_]*.js')
+    .pipe(plumber())
+    .pipe(rename({dirname: ''}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/js'))
+    .pipe(browserSync.reload( {stream: true} ))
+  done();
+});
+
+gulp.task('images', (done) => {
+  gulp
+    .src('src/images/*')
+    .pipe(plumber())
+    .pipe(changed('./www/images'))
+    .pipe(rename({dirname: ''}))
+    .pipe(imagemin())
+    .pipe(gulp.dest('./www/images'))
+    .pipe(browserSync.reload( {stream: true} ))
+  done();
+})
+
+gulp.task('default', gulp.series('pug', 'styles', 'script', 'images', 'browserSync'));
